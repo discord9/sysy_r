5b36f79c476c8f33aa25007d75ce18cb97e1356b
Discord9 <discord9@163.com>
2022-04-07 16:35:04 +0800

    add comp_unit

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,34 @@
 //! Abstract Syntax Tree
 //! 
 use crate::cst::{SyntaxNode};
-use crate::syntax::SyntaxKind;
+use crate::syntax::SyntaxKind as Kind;
+
+macro_rules! ast_node {
+    ($ast:ident, $kind:ident) => {
+        #[derive(PartialEq, Eq, Hash)]
+        #[repr(transparent)]
+        struct $ast(SyntaxNode);
+        impl $ast {
+            #[allow(unused)]
+            fn cast(node: SyntaxNode) -> Option<Self> {
+                if node.kind() == $kind {
+                    Some(Self(node))
+                } else {
+                    None
+                }
+            }
+        }
+    };
+}
+
+struct comp_unit(SyntaxNode);
+impl comp_unit{
+    #[allow(unused)]
+    fn cast(node: SyntaxNode) -> Option<Self> {
+        if node.kind() == Kind::CompUnit {
+            Some(Self(node))
+        } else {
+            None
+        }
+    }
+}
