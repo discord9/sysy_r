4289189a166196a8b9a768a3f99d516df30db848
Discord9 <discord9@163.com>
2022-05-09 23:47:40 +0800

    BoolOp short circuit support

--- src/backend/inkwell.rs
+++ src/backend/inkwell.rs
@@ -41,7 +41,7 @@ pub enum CompileError {
 
 use crate::frontend::ast::Expr;
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
-    /// Gets a defined function given its name.
+    
     #[inline]
     fn f32_type(&self) -> FloatType<'ctx> {
         self.context.f32_type()
@@ -50,13 +50,14 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
     fn i32_type(&self) -> IntType<'ctx> {
         self.context.i32_type()
     }
+    /// Gets a defined function given its name.
     #[inline]
     fn get_function(&self, name: &str) -> Option<FunctionValue<'ctx>> {
         self.module.get_function(name)
     }
 
     /// cast a `i32` to `f32` using `SIToFP` instr, or directly return `FloatValue` if already is a `BasicValueEnum::FloatValue`, if type is none above, return `None`
-    fn cast2f32(&self, val: BasicValueEnum<'ctx>) -> Option<FloatValue<'ctx>> {
+    fn force_cast_f32(&self, val: BasicValueEnum<'ctx>) -> Option<FloatValue<'ctx>> {
         match val {
             BasicValueEnum::IntValue(vi) => {
                 let res = self
@@ -76,6 +77,15 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
         }
     }
 
+    fn force_cast_i32(&self, val: BasicValueEnum<'ctx>) -> Option<IntValue<'ctx>>{
+        match val{
+            BasicValueEnum::FloatValue(v)=>{
+                Some(self.builder.build_float_to_signed_int(v, self.i32_type(), "forced_int"))
+            }
+            BasicValueEnum::IntValue(v)=>Some(v),
+            _=>None
+        }
+    }
     /// Returns the `FunctionValue` representing the function being compiled.
 
     #[inline]
@@ -183,8 +193,8 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
                 //use inkwell::values::{FloatMathValue, FloatValue, IntMathValue, IntValue};
                 if use_float {
                     // use float cast!
-                    let lhs: FloatValue = self.cast2f32(lhs).unwrap();
-                    let rhs: FloatValue = self.cast2f32(rhs).unwrap();
+                    let lhs: FloatValue = self.force_cast_f32(lhs).unwrap();
+                    let rhs: FloatValue = self.force_cast_f32(rhs).unwrap();
                     match *op {
                         SyntaxKind::OpAdd => {
                             Ok(self.builder.build_float_add(lhs, rhs, "tmpadd").into())
@@ -224,6 +234,83 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
                         }
                         _ => Err(CompileError::UndefinedBinOp(format!("{:?}", op))),
                     }
+                }
+            }
+            ExprKind::BoolOp { op, args } => {
+                use inkwell::IntPredicate;
+                use crate::syntax::SyntaxKind;
+                match *op{
+                    SyntaxKind::OpAnd =>{
+                        let parent = self.fn_value();
+                        let mut and_chain = Vec::new();
+                        and_chain.push(self.context.append_basic_block(parent, "and"));
+                        let cont_bb = self.context.append_basic_block(parent, "cont");
+                        self.builder.position_at_end(and_chain[0]);
+                        let mut cond = self.context.bool_type().const_int(0, false);
+                        let zero = self.context.bool_type().const_int(0, false);
+                        for arg in args{
+                            // Short circuit(difficult!)
+                            let val = self.compile_expr(arg)?;
+                            let val = self.force_cast_i32(val).unwrap();
+                            cond = self.builder.build_int_compare(IntPredicate::NE, val, zero, "and");
+                            let and_bb = and_chain.last().unwrap().to_owned();
+                            let append_bb = self.context.append_basic_block(parent, "and");
+                            and_chain.push(append_bb);
+                            self.builder.position_at_end(and_bb);
+                            self.builder.build_conditional_branch(cond, append_bb, cont_bb);
+                            self.builder.position_at_end(append_bb);
+                        }
+        
+                        // emit merge block
+                        self.builder.position_at_end(cont_bb);
+                        let phi = self.builder.build_phi(self.context.bool_type(), "andtmp");
+                        let mut is_last = true;
+                        for and_bb in and_chain.iter().rev(){
+                            if is_last{
+                                phi.add_incoming(&[(&cond, *and_bb)])
+                            }else{
+                                phi.add_incoming(&[(&zero, *and_bb)])
+                            }
+                        }
+                        Ok(phi.as_basic_value())
+                    }
+                    SyntaxKind::OpOr => {
+                        let parent = self.fn_value();
+                        let mut or_chain = Vec::new();
+                        let or_bb = self.context.append_basic_block(parent, "or");
+                        or_chain.push(or_bb);
+                        let cont_bb = self.context.append_basic_block(parent, "cont");
+                        self.builder.position_at_end(or_bb);
+                        let mut cond = self.context.bool_type().const_int(0, false);
+                        let zero = self.context.bool_type().const_int(0, false);
+                        for arg in args{
+                            // Short circuit(difficult!)
+                            let val = self.compile_expr(arg)?;
+                            let val = self.force_cast_i32(val).unwrap();
+                            cond = self.builder.build_int_compare(IntPredicate::NE, val, zero, "or");
+                            let or_bb = or_chain.last().unwrap().to_owned();
+                            let append_bb = self.context.append_basic_block(parent, "or");
+                            or_chain.push(append_bb);
+                            self.builder.position_at_end(or_bb);
+                            self.builder.build_conditional_branch(cond, cont_bb, append_bb);
+                            self.builder.position_at_end(append_bb);
+                        }
+        
+                        // emit merge block
+                        self.builder.position_at_end(cont_bb);
+                        let phi = self.builder.build_phi(self.context.bool_type(), "ortmp");
+                        let mut is_last = true;
+                        let one = self.context.bool_type().const_int(1, false);
+                        for or_bb in or_chain.iter().rev(){
+                            if is_last{
+                                phi.add_incoming(&[(&cond, *or_bb)])
+                            }else{
+                                phi.add_incoming(&[(&one, *or_bb)])
+                            }
+                        }
+                        Ok(phi.as_basic_value())
+                    }
+                    _ => unreachable!()
                 }
             }
             _ => unreachable!(),
